ffb72d39b2d85409f0ba97dc6bd73c9a7f56e88e
Pedro <pedrohpvaldes@icloud.com>
2025-08-01 11:56:40 -0300

    first model test

--- test_model.py
+++ test_model.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 # Carregar modelo treinado
 model = tf.keras.models.load_model("model/az_letters_cnn.h5")
 
+# Carregar conjuntos de teste salvos
+X_test = np.load("model/X_test.npy")
+y_test = np.load("model/y_test.npy")
+
 # Função para prever letra
 def predict_letter(image):
     prediction = model.predict(np.expand_dims(image, axis=0))
-    return chr(np.argmax(prediction) + ord('A'))  # Converte índice em letra
+    return chr(np.argmax(prediction) + ord('A'))
 
-# Testar com algumas imagens do conjunto de teste
+# Testar com algumas imagens
 for i in range(5):
     img = X_test[i]
-    true_label = chr(y_test[i] + ord('A'))
+    true_label = chr(int(y_test[i]) + ord('A'))  # Conversão para int
     predicted_label = predict_letter(img)
     
     plt.imshow(img.squeeze(), cmap="gray")
